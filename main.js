96dc8127950c325dd47444dcf2f778a69578898f
Drishti2009 <84116313+Drishti2009@users.noreply.github.com>
2022-06-21 13:59:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function  read_story1() {
+    window.location = "story1.html";
+}
+
+function  read_story2() {
+    window.location = "story2.html";
+}
+
+function  read_story3() {
+    window.location = "story3.html";
+}
+
+function  read_story4() {
+    window.location = "story4.html";
+}
+
+function  read_story5() {
+    window.location = "story5.html";
+}
+
+function  read_story6() {
+    window.location = "story6.html";
+}
+
+function  read_story7() {
+    window.location = "story7.html";
+}
